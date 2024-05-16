f381ea85cdfce2beec11e6631ea8424115984905
ReturnerNaGithub <damsan1@gmail.com>
2024-05-16 19:08:17 +0200

    pik

--- pages/news.tsx
+++ pages/news.tsx
@@ -10,7 +10,7 @@ export default function News () {
       <Flex direction={"column"} h={"100vh"} justifyContent={"top"}>
       <Heading my={"40px"}>News: This game was made for Chainlink Hackathon 2024</Heading>
       <Stack spacing={3}>
-      <Text fontSize='2xl'>Wellcome players and judges to test funny game. Injoj and don,t tell anyone.</Text>
+      <Text fontSize='2xl'>Wellcome players and judges to test funny game- Enjoy and don,t tell anyone.</Text>
       </Stack>
       
 <Link href="/">
